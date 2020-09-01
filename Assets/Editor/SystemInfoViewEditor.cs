f9abd6e664a0ca366dbbd7242980d8686e92e7b0
Koki Ibukuro <koki.ibukuro@gmail.com>
2020-09-01 12:58:04 +0200

    Show system info

---
+++ Assets/Editor/SystemInfoViewEditor.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SystemInfoView))]
+public sealed class SystemInfoViewEditor : Editor
+{
+    private SerializedProperty label;
+    // private SerializedProperty props;
+    private bool foldoutShowProps = true;
+    private bool foldoutHideProps = true;
+    private static string filter = "";
+
+    private void OnEnable()
+    {
+        label = serializedObject.FindProperty("label");
+        // props = serializedObject.FindProperty("props");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(label);
+
+        filter = EditorGUILayout.TextField("Filter Property", filter);
+
+        var props = ((SystemInfoView)target).Properties.AsEnumerable();
+
+        if (!string.IsNullOrWhiteSpace(filter))
+        {
+            props = props.Where(p => p.propName.ToLower().Contains(filter.ToLower()));
+        }
+        var showProps = props.Where(p => p.isShow);
+        var hideProps = props.Where(p => !p.isShow);
+
+        // Undo check
+        bool isPropChanged = false;
+
+        // Show
+        foldoutShowProps = EditorGUILayout.BeginFoldoutHeaderGroup(foldoutShowProps, "Show Props");
+        if (foldoutShowProps)
+        {
+            foreach (var prop in showProps)
+            {
+                isPropChanged = isPropChanged | ShowProp(prop);
+            }
+        }
+        EditorGUILayout.EndFoldoutHeaderGroup();
+
+        // Hide
+        foldoutHideProps = EditorGUILayout.BeginFoldoutHeaderGroup(foldoutHideProps, "Hide Props");
+        if (foldoutHideProps)
+        {
+            foreach (var prop in hideProps)
+            {
+                isPropChanged = isPropChanged | ShowProp(prop);
+            }
+        }
+        EditorGUILayout.EndFoldoutHeaderGroup();
+
+        if (isPropChanged)
+        {
+            Undo.RecordObject(target, "Changed Property Show/Hide");
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    /// <summary>
+    /// Show Property in Editor
+    /// </summary>
+    /// <param name="prop"></param>
+    /// <returns>Is the property changed</returns>
+    private static bool ShowProp(SystemInfoView.Prop prop)
+    {
+        bool before = prop.isShow;
+        bool after = EditorGUILayout.ToggleLeft(prop.displayName, prop.isShow);
+        prop.isShow = after;
+        return before == after;
+    }
+}
--- Assets/Runtime/SystemInfoView.cs
+++ Assets/Runtime/SystemInfoView.cs
@@ -4,22 +4,141 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Reflection;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SystemInfoView : MonoBehaviour
-{
-    [SerializeField] Text label = null;
-    [SerializeField] TextAsset pciDeviceTsv = null;
+/// <summary>
+/// Show selected list of SustemInfo values
+/// https://docs.unity3d.com/ScriptReference/SystemInfo.html
+/// </summary>
 
-    private void Start()
+[ExecuteInEditMode]
+public sealed class SystemInfoView : MonoBehaviour
+{
+    [System.Serializable]
+    public class Prop
     {
-        var sb = new System.Text.StringBuilder();
-        sb.AppendLine($"Device Name: {SystemInfo.deviceName}");
-        sb.AppendLine($"Device Model: {SystemInfo.deviceModel}");
-        sb.AppendLine($"Graphics Device Vendor: {SystemInfo.graphicsDeviceVendor}");
-        label.text = sb.ToString();
+        public string propName;
+        public string displayName;
+        public bool isShow;
+        public MemberTypes memberType;
+
+        public bool IsValid => !string.IsNullOrWhiteSpace(propName)
+                            && !string.IsNullOrWhiteSpace(displayName);
+
+
+        public object GetValue()
+        {
+            if (string.IsNullOrWhiteSpace(propName))
+            {
+                return $"Not Valid";
+            }
+
+            var type = typeof(SystemInfo);
+            switch (memberType)
+            {
+                case MemberTypes.Field:
+                    return type.GetField(propName).GetValue(null);
+                case MemberTypes.Property:
+                    return type.GetProperty(propName).GetValue(null);
+                case MemberTypes.Method:
+                    var method = type.GetMethod(propName);
+                    if (method.GetParameters().Length == 0)
+                    {
+                        return method.Invoke(null, new object[]{});
+                    }
+                    else {
+                        return $"Method with Parameter is not suppoerted";
+                    }
+            }
+
+            return $"Not Valid: {memberType}";
+        }
     }
 
+    [SerializeField] Text label = null;
+    [SerializeField] Prop[] props;
+
+    public Prop[] Properties => props;
+
+    private int lastPropCount;
+    private void Start()
+    {
+        if (label != null)
+        {
+            label.text = BuildInfo();
+        }
+        lastPropCount = props.Length;
+    }
+
+    private void Update()
+    {
+        if (label == null || lastPropCount == props.Length)
+        {
+            return;
+        }
+        label.text = BuildInfo();
+        lastPropCount = props.Length;
+    }
+
+    public string BuildInfo()
+    {
+
+        var sb = new System.Text.StringBuilder();
+        foreach (var prop in props)
+        {
+            if (prop.isShow)
+            {
+                sb.AppendLine($"- {prop.displayName}: {prop.GetValue()}");
+            }
+        }
+        return sb.ToString();
+    }
+
+
+    #region Editor Code
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        if (props == null
+        || props.Length == 0
+        || props.Any(p => !p.IsValid))
+        {
+            RebuildProps();
+        }
+    }
+
+    private void RebuildProps()
+    {
+        Debug.Log("Rebuild Props");
+        props = typeof(SystemInfo)
+            .GetMembers(BindingFlags.Public | BindingFlags.Static)
+            .Select(info =>
+            {
+                Debug.Log($"{info} MemberType: {info.MemberType}");
+                return new Prop()
+                {
+                    propName = info.Name,
+                    displayName = PropToDisplayName(info.Name),
+                    isShow = false,
+                    memberType = info.MemberType,
+                };
+            }).ToArray();
+    }
+
+    // TODO fix regex
+    private static readonly Regex CapcalCaseRegex = new Regex(@"(?<!^)(?=[A-Z])");
+    private static string PropToDisplayName(string propName)
+    {
+        string name = propName.Replace("get_", "");
+        name = Char.ToUpper(name.First()) + name.Substring(1);
+        var words = CapcalCaseRegex.Split(name);
+        return string.Join(" ", words);
+    }
+
+#endif // UNITY_EDITOR
+    #endregion // Editor Code
 
 }
