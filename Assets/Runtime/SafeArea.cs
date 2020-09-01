c00f4dc20501d0744254e1606fa34ef739ca1997
Koki Ibukuro <koki.ibukuro@gmail.com>
2020-09-01 10:05:35 +0200

    first commit

---
+++ Assets/Runtime/SafeArea.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+[ExecuteInEditMode]
+public class SafeArea : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        var rt = GetComponent<RectTransform>();
+        UpdateSafeArea(rt, Screen.safeArea);
+    }
+
+    private void OnDisable()
+    {
+
+    }
+
+    private void UpdateSafeArea(RectTransform rt, Rect safeArea)
+    {
+        // Convert safe area rectangle from absolute pixels to normalised anchor coordinates
+        Vector2 anchorMin = safeArea.position;
+        Vector2 anchorMax = safeArea.position + safeArea.size;
+        anchorMin.x /= Screen.width;
+        anchorMin.y /= Screen.height;
+        anchorMax.x /= Screen.width;
+        anchorMax.y /= Screen.height;
+        rt.anchorMin = anchorMin;
+        rt.anchorMax = anchorMax;
+    }
+}
---
+++ Assets/Runtime/SystemInfoView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SystemInfoView : MonoBehaviour
+{
+    [SerializeField] Text label = null;
+    [SerializeField] TextAsset pciDeviceTsv = null;
+
+    private void Start()
+    {
+        var sb = new System.Text.StringBuilder();
+        sb.AppendLine($"Device Name: {SystemInfo.deviceName}");
+        sb.AppendLine($"Device Model: {SystemInfo.deviceModel}");
+        sb.AppendLine($"Graphics Device Vendor: {SystemInfo.graphicsDeviceVendor}");
+        label.text = sb.ToString();
+    }
+
+
+}
